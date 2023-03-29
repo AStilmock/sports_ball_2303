a9ad658ed61f9e3d2bc61816d77b4caf2bed0006
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-03-29 09:59:21 -0600

    Complete iteration 1 tests

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,14 @@
 class Player
-  attr_reader :name, :monthly_cost, :contract_length
+  attr_reader :name, :monthly_cost, :contract_length, :nickname
   def initialize(name, monthly_cost, contract_length)
     @name = name
     @monthly_cost = monthly_cost
     @contract_length = contract_length
+    @nickname = nil
   end
 
   def first_name 
     first_name = name.split[0]
-    # @player.name.split[0]
   end
 
   def last_name
@@ -18,5 +18,10 @@ class Player
   def total_cost
     total_cost = monthly_cost * contract_length
   end
+
+  def set_nickname!(new_nickname)
+    @nickname = new_nickname
+  end
+
 
 end

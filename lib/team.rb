643b05509cc930d3cd79c421930a0f6d40923887
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-03-29 11:03:17 -0600

    Add code and tests for iteration 3

--- lib/team.rb
+++ lib/team.rb
@@ -12,5 +12,30 @@ class Team
     @player_count += 1
   end
   
+  def long_term_players
+    long_term_players = roster.find_all do |player|
+      player.contract_length > 24
+    end
+  end
+  
+  def short_term_players
+    short_term_players = roster.find_all do |player|
+      player.contract_length <= 24
+    end
+  end
+
+  def total_value
+    total_value = roster.map do |player|
+      player.total_cost
+    end
+      total_value.sum
+  end
+    
+
+  def details
+    details = #{@team.total_value, team.player_count}
+  end
+
+  
 
 end
